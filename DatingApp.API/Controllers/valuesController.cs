cf6183c609a99decbeae1f48d71fd646ed5a5f44
ChandanKumar2 <chandan50191@outlook.com>
2019-12-09 18:39:30 +0530

    initial commit

---
+++ DatingApp.API/Controllers/valuesController.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        // GET api/values
+        [HttpGet]
+        // IActionResult is for Http requests
+        // Asnyc request are great to use because it will not block the code and allow other users to send a reuest
+        // for async methos Task is the key word
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.values.ToListAsync();
+            return Ok(values);
+        }
+            
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+           var value =await _context.values.FirstOrDefaultAsync(x => x.Id == id);
+           return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
